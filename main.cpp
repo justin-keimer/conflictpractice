8db3ee4b285eeff46906af363e2f33d7ed5a17de
justin-keimer <jkeimer@nic.edu>
2024-10-23 17:20:04 +0000

    add function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b);
+
 int main() {
 
     cout <<"My name is Tom\n";
     cout <<"hello world\n";
+    cout << add(5, 7)<<endl;
     return 0;
+}
+
+int add(int a , int b) {
+    return a + b;
 }
