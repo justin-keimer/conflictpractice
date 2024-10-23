87793d4c1ed5cb80eec6d3612994c207450559a8
Justin Keimer <jkeimer@nic.edu>
2024-10-23 10:20:12 -0700

    message function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
+void say(string word);
 
 int main() {
 
     cout <<"hello world\n";
+    say("Hello function");
     return 0;
+}
+
+void say(string word) {
+    cout << word;
 }
